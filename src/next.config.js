ac87c07aa00ab895baa1df3396fda895e1774852
MINJE-98 <jmj012100@gmail.com>
2022-06-19 15:50:28 +0900

    refactor: 폴더 구조 변경

--- src/next.config.js
+++ src/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    TEST: process.env.TEST,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
